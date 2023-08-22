1ec27849472596837bd2a3b59f52129779e700bf
Melek Gülsever <melek.guelsever@nentgroup.com>
2023-08-22 17:27:27 +0200

    Cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
 import Card from '../components/Card/Card';
 import Heading from '../components/Heading/Heading';
+
 export default function Index() {
-
-
-  // TODO: Instead of using this hardcoded data variable, fetch data from API endpoint '/api/cards'. The data structure is the same.
 
   const [data, setData] = useState(null);
 
@@ -17,21 +15,6 @@ export default function Index() {
 
     fetchData();
   }, []);
-
-  // const data = [
-  //   {
-  //     id: '1',
-  //     heading: 'This is hardcoded',
-  //     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  //     icon: 'house',
-  //   },
-  //   {
-  //     id: '2',
-  //     heading: 'Not the correct data',
-  //     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  //     icon: 'grade',
-  //   },
-  // ];
 
   return (
     <div>
