387ecdeee3629f33b36139fa8b4902de5200106a
Melek Gülsever <melek.guelsever@nentgroup.com>
2023-08-24 18:53:41 +0200

    Cleanup

--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -2,7 +2,6 @@ import React from 'react';
 import s from './styles.module.css';
 
 const Heading = () => {
-    // const svg = `/icons/${icon}.svg`;
     const heading = 'So much things to say right now - I know';
 
     return (
