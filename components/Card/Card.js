0506219aeac47c996837ea8674687efbeea11fc3
Melek Gülsever <melek.guelsever@nentgroup.com>
2023-08-24 18:52:26 +0200

    Cleanup

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import s from './styles.module.css';
-import Icon from '../Icon/Icon.js';
 
 const Card = ({ card }) => {
-  const { id, heading, text, icon } = card;
+  const { heading, text } = card;
   return (
     <div className={s.card}>
-      <p><strong>id:</strong> {id}</p>
-      <p><strong>heading:</strong> {heading}</p>
-      <p><strong>text:</strong> {text}</p>
-      <p><strong>icon:</strong> {icon}</p>
-      <Icon icon={icon} />
-      {/* <img src={`/icons/${icon}.svg`} /> */}
+      <p><strong>{heading}</strong></p>
+      <p className={s.p}>{text}</p>
     </div>
   )
 };
