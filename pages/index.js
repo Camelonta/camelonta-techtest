9e0dc4776474ccaf709a52f322a240a4a953eaa8
Melek Gülsever <melek.guelsever@nentgroup.com>
2023-08-24 22:22:06 +0200

    nextjs magic, fetch data at build time

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,9 @@ import { useState, useEffect } from 'react';
 import Heading from '../components/Heading/Heading';
 import CardBlock from '../components/CardBlock/CardBlock';
 import s from './styles.module.css';
+import React from 'react';
 
-export default function Index() {
-
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('./api/cards');
-      const jsonData = await response.json();
-      setData(jsonData);
-    }
-
-    fetchData();
-  }, []);
-
+function Index({ data }) {
 
   return (
     <div className={s.container}>
@@ -28,3 +16,33 @@ export default function Index() {
 
   )
 }
+
+
+export async function getStaticProps() {
+  try {
+    const response = await fetch('http://localhost:3000/api/cards');
+    const data = await response.json();
+
+    if (!data) {
+      return {
+        notFound: true,
+      };
+    }
+    return {
+      props: {
+        data,
+      },
+      revalidate: 60,
+    };
+
+  } catch (error) {
+    console.error('Unable to fetch data', error);
+    return {
+      props: {
+        data: [],
+      },
+    };
+  }
+}
+
+export default Index;
